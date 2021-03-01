e39543ed259bc6ce452499894b0bdca3e5a7617a
rnkvogel <rvogel@influxis.com>
2021-03-01 11:24:40 -0800

    Update viewer360.js

--- player/js/viewer360.js
+++ player/js/viewer360.js
@@ -5,10 +5,8 @@
 
 
   let params     = new URLSearchParams(document.location.search.substring(1));
-  //let accountId  = params.get('accountId');
-  let accountId = "LZsuF8";
-  //let streamName = params.get('streamName');
-  let streamName = "klmw71ty";
+  let accountId  = params.get('account');
+  let streamName = params.get('id');
   let subToken = params.get('token');// SubscribingToken - placed here for ease of testing, should come from secure location. (php/nodejs)
   let lang = document.getElementById('subtitles');
   let poster = "images/starting.png";
